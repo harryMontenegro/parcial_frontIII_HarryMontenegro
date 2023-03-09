f3d4c41940a32bc627c9474850a8cdbb634fd3ee
harrymontenegro <harryjm018@hotmail.com>
2023-03-08 19:19:22 -0500

    plantilla inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
+  //Aqui deberias agregar los estados y los handlers para los inputs
 
   return (
     <div className="App">
+      <h1>Elige un color</h1>
+      <form>{/* aqui deberias escribir tu codigo */}</form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
